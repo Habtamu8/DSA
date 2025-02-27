4d7788117bda240ad3ab0a243532b06c2e4d4107
habtamu8 <habtamuwoldeamanuel8@gmail.com>
2025-02-27 20:12:03 +0300

    circular single linked list

---
+++ circular single linked list.java
@@ -0,0 +1,33 @@
+public class Main {
+  
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String [] args) {
+      Node firstnode = new Node(2);
+      Node secondnode = new Node(3);
+      Node thirdnode = new Node(12);
+      Node fourthnode = new Node (20);
+
+      Node currentNode = firstnode;
+      Node lastnode = fourthnode;
+
+      System.out.print( currentNode.data + " ->");
+      currentNode = currentNode.next;
+
+      while(currentNode != lastnode){
+        system.out.print(currentNode.data)
+        currentNode = currentNode.next;
+
+      }
+
+    }
+
+}
