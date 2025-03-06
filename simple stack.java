85fb3898fcc91943384b578a697250736b723628
habtamu8 <habtamuwoldeamanuel8@gmail.com>
2025-03-06 20:32:02 +0300

    simple stack

---
+++ simple stack.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+public class Main{
+    public static void main(String [] args){
+
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("Minectaft");
+        stack.push("PS5");
+        stack.push("Mine");
+        stack.push("Mi");
+        stack.push("draft");
+  
+System.out.print(stack);
+       
+    }
+}
