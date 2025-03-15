930ff8e957a2dedc1bf10603aa9c47078fb1a515
habtamu8 <habtamuwoldeamanuel8@gmail.com>
2025-03-15 16:41:23 +0300

    bubble sort

--- Bubble sort.java
+++ Bubble sort.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public  static void main(String[] args) {
         int[] my_array = {64, 34, 25, 12, 22, 11, 90, 5};
         int n = my_array.length;
         for(int i = 0; i < n-1; i++) {
