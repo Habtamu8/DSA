8242e0a2db408d9665a9fea01c0848985d1aa1d0
0113492088H@%c <habtamuwoldeamanuel8@gmail.com>
2025-05-07 12:44:08 +0300

    fib

--- Fibonnchi .java
+++ Fibonnchi .java
@@ -8,7 +8,7 @@ public class Main {
             prev2 = prev1;
             prev1 = newFibo;
             count += 1;
-            and again
+             i will always love u
             fibonacci (prev1, prev2);
         } else {
             return;
